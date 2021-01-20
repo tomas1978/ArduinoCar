6ff3033089767454bc4fd86d404b072ee701217a
tomas1978 <tomas.nilsson78@gmail.com>
2021-01-20 11:43:50 +0100

    Connected H-bridge and code to test motor control

--- car_controller/car_controller.ino
+++ car_controller/car_controller.ino
@@ -4,10 +4,14 @@ Servo carServo;
 
 int servoPotPin = 0;
 int servoPotValue;
+int inputPin1=2;
+int inputPin2=3;
 
 void setup()
 {
   carServo.attach(9);
+  pinMode(inputPin1,OUTPUT);
+  pinMode(inputPin2,OUTPUT);
 }
 
 void loop()
@@ -16,4 +20,20 @@ void loop()
   servoPotValue=map(servoPotValue, 0, 1023, 0, 180);
   carServo.write(servoPotValue);
   delay(15);
+  
+  digitalWrite(inputPin1,HIGH);
+  digitalWrite(inputPin2,LOW);
+  delay(2000);
+  
+  digitalWrite(inputPin1,LOW);
+  digitalWrite(inputPin2,LOW);
+  delay(1000);
+  
+  digitalWrite(inputPin1,LOW);
+  digitalWrite(inputPin2,HIGH);
+  delay(2000);
+  
+  digitalWrite(inputPin1,LOW);
+  digitalWrite(inputPin2,LOW);
+  delay(1000);
 }
