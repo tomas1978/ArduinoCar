ac074cf3dcdabaa759108b2c6784b1aac014a475
tomas1978 <tomas.nilsson78@gmail.com>
2021-01-21 15:09:21 +0100

    Added speed control
    Implemeted speed control by connecting a potentiometer to analog pin 1, reading the value, mapping it to range 1..255 and using analogRead to set the voltage of the enable pin on the H-bridge

--- car_controller/car_controller.ino
+++ car_controller/car_controller.ino
@@ -4,14 +4,18 @@ Servo carServo;
 
 int servoPotPin = 0;
 int servoPotValue;
+int speedPotPin = 1;
 int inputPin1=2;
 int inputPin2=3;
+int motorSpeed=0;
+int enablePin=5;
 
 void setup()
 {
   carServo.attach(9);
   pinMode(inputPin1,OUTPUT);
   pinMode(inputPin2,OUTPUT);
+  pinMode(enablePin,OUTPUT);
 }
 
 void loop()
@@ -20,6 +24,10 @@ void loop()
   servoPotValue=map(servoPotValue, 0, 1023, 0, 180);
   carServo.write(servoPotValue);
   delay(15);
+  
+  motorSpeed=analogRead(speedPotPin);
+  motorSpeed=map(motorSpeed, 0, 1023, 0, 255);
+  analogWrite(enablePin,motorSpeed);
   
   digitalWrite(inputPin1,HIGH);
   digitalWrite(inputPin2,LOW);
@@ -36,4 +44,4 @@ void loop()
   digitalWrite(inputPin1,LOW);
   digitalWrite(inputPin2,LOW);
   delay(1000);
-}
+}t
